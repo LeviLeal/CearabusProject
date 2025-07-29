e8018bc207db2c84a9b445ce11822da7ee28431a
LeviLeal <levyfleal@gmail.com>
2025-07-29 20:24:15 -0300

    Adicionado fotos de usuario e comprovante

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ const handlebars = require("express-handlebars")
 const app = express()
 const path = require("path")
 const session = require("express-session")
-// const checarLogin = require("./middlewares/checarLogin")
+const multer = require("multer")
 
 const { Client } = require("pg")
 const { error } = require("console")
@@ -14,6 +14,20 @@ const client = new Client({
     password: "",
     database: "cearabus"
 })
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        const fullPath = path.join(__dirname, '..', 'public', 'img', 'aluno');
+        cb(null, fullPath);
+    },
+    filename: (req, file, cb) => {
+        const ext = path.extname(file.originalname);
+        const filename = `${Date.now()}-${file.fieldname}${ext}`;
+        cb(null, filename);
+    }
+});
+
+const upload = multer({ storage });
 
 client.connect().then(() => console.log("connected"))
 
@@ -36,18 +50,21 @@ app.set("views", path.join(__dirname, "views"))
 
 app.use(express.static(path.join(__dirname, "..", "public")))
 
+app.use((req, res, next) => {
+    res.locals.admin = req.session.admin || false;
+    next();
+});
+
 // Rotas
 
 // Rota raiz, se logado vai para o dashboard, se nao, deve logar ou se cadastar
 app.get("/", (req, res) => {
-    console.log(req.session.logado)
+    let hideNavbar = true
     if (req.session.logado) {
-        res.render("aluno/dashboard")
+        res.redirect("/rotas/")
+    } else {
+        res.render("unlogged/login", { hideNavbar })
     }
-    else
-        res.render("unlogged/login")
-
-    console.log(path.join(__dirname, "public"))
 })
 
 // POST logar
@@ -58,8 +75,8 @@ app.post("/logar/", (req, res) => {
     client.query(`SELECT * FROM usuarios WHERE cpf = '${cpf}' AND senha = '${senha}'`, (err, result) => {
         if (!err) {
             if (result.rows[0]) {
-                console.log("logado")
                 req.session.logado = true;
+                req.session.user_id = result.rows[0].id
                 res.redirect("/")
             }
         } else {
@@ -69,31 +86,140 @@ app.post("/logar/", (req, res) => {
     })
 })
 
-// GET cadastrar usuario
-app.get("/cadastro/", (req, res) => {
-    res.render("unlogged/cadastro")
+// GET logar administrador
+
+app.get("/logar_adm/", (req, res) => {
+    let hideNavbar = true
+    let admin = true
+    res.render("unlogged/login_adm", { hideNavbar, admin })
 })
 
-// POST cadastrar usuario
-app.post("/cadastro/", (req, res) => {
-    console.log(req.body)
+// POST logar administrador
 
-    let query = `INSERT INTO usuarios (nome, email, cpf, matricula, numero_celular, foto, comprovante_faculdade, senha)
-VALUES ('${req.body.nome}', '${req.body.email}', '${req.body.cpf}', '${req.body.matricula}', '${req.body.celular}', '${req.body.foto}', '${req.body.comprovante}', '${req.body.senha}')`
+app.post("/logar_adm/", (req, res) => {
+    let email = req.body.email
+    let senha = req.body.senha
 
-    client.query(query, (err, res) => {
-        if (!err)
-            console.log("deu certo papai")
-        else
-            console.log("hoje nao")
-
+    client.query(`SELECT * FROM admins WHERE username = '${email}' AND password = '${senha}'`, (err, result) => {
+        if (!err) {
+            console.log(result.rows)
+            if (result.rows[0]) {
+                req.session.logado = true
+                req.session.admin = true
+                res.redirect("/")
+            } else {
+                res.redirect("/logar_adm/")
+            }
+        } else {
+            console.log(error)
+            res.redirect("/logar_adm/")
+        }
     })
+})
+
+// POST deslogar
+app.get("/deslogar/", (req, res) => {
+    req.session.logado = false
+    req.session.admin = false
     res.redirect("/")
 })
 
-// recuperar senha
+// GET cadastrar usuario
+app.get("/cadastro/", (req, res) => {
+    let hideNavbar = true
+    res.render("unlogged/cadastro", { hideNavbar })
+})
+
+// POST cadastrar usuario
+app.post("/cadastro/", upload.fields([
+    { name: 'foto', maxCount: 1 },
+    { name: 'comprovante', maxCount: 1 }
+]), (req, res) => {
+
+    const nomeFoto = req.files['foto'] ? req.files['foto'][0].filename : null;
+    const nomeComprovante = req.files['comprovante'] ? req.files['comprovante'][0].filename : null;
+
+        let query = `INSERT INTO usuarios (nome, email, cpf, matricula, numero_celular, foto, comprovante_faculdade, senha, curso, instituicao, parada)
+    VALUES ('${req.body.nome}', '${req.body.email}', '${req.body.cpf}', '${req.body.matricula}', '${req.body.celular}', '${nomeFoto}', '${nomeComprovante}', '${req.body.senha}', '${req.body.curso}', '${req.body.instituicao}', '${req.body.ponto}')`
+        client.query(query, (err, res) => {
+            if (!err) {
+
+                console.log("deu certo papai")
+            }
+            else
+                console.log("hoje nao")
+
+        })
+    res.redirect("/")
+})
+
+// GET recuperar senha
 app.get("/recuperar_senha/", (req, res) => {
-    res.render("unlogged/recuperar_senha")
+    let hideNavbar = true
+    res.render("unlogged/recuperar_senha", { hideNavbar })
+})
+
+// GET rotas
+app.get("/rotas/", (req, res) => {
+    res.render("aluno/rotas")
+})
+
+// GET carteirinha
+app.get("/carteira/", (req, res) => {
+
+    id = req.session.user_id 
+    // id = 5
+    client.query(`SELECT * FROM usuarios WHERE id = '${id}'`, (err, result) => {
+        if (!err) {
+            if (result.rows[0]) {
+                let aluno = result.rows[0]
+                res.render("aluno/carteira", { aluno })
+            }
+        } else
+            console.log(error)
+    })
+
+})
+
+// GET aprovar carteirinha
+app.get("/aprovar_carteira/", (req, res) => {
+    client.query(`SELECT * FROM usuarios WHERE aprovado = FALSE ORDER BY id DESC`, (err, result) => {
+        if (!err) {
+            if (result.rows) {
+                let alunos = result.rows
+                res.render("admin/aprovar_carteira", { alunos })
+            }
+        } else
+            console.log(error)
+    })
+
+})
+
+// POST julgar carteirinha
+app.post("/julgar_carteirinha/", (req, res) => {
+    let acao = req.body.acao
+    let id = req.body.id
+
+    if (acao == "aprovar") {
+        client.query(`UPDATE usuarios SET aprovado = true WHERE id = ${id}`, (err, result) => {
+            if (!err) {
+                if (result.rows)
+                    res.redirect("/aprovar_carteira/")
+            } else
+                console.log(error)
+        })
+    } else if (acao = "reprovar") {
+        client.query(`DELETE FROM usuarios WHERE id = ${id}`, (err, result) => {
+            if (!err) {
+                if (result.rows) {
+                    console.log(result.rows)
+                    res.redirect("/aprovar_carteira/")
+                }
+            } else
+                console.log(error)
+        })
+    }
+
 })
 
 const PORT = "5173"
--- src/middlewares/checarLogin.js
+++
@@ -1,8 +0,0 @@
-function checarLogin(req, res, next) {
-  if (req.session.logado) {
-    return next();
-  }
-  return res.status(401).send('Faça login');
-}
-
-module.exports = checarLogin;
